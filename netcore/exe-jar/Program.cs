5c8be407c729268d317bb00ddc7a5bb3f6cc9e40
Ricardo Paes <ricardo@likesistemas.com.br>
2021-02-04 17:49:27 -0300

    Compiling using .NET Core 5.0

---
+++ netcore/exe-jar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace exe_jar
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            bool instanceCountOne = false;
+            using (Mutex mtex = new Mutex(true, "MyRunningApp", out instanceCountOne))
+            {
+                if (instanceCountOne)
+                {
+                    var process = new Process()
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "java.exe",
+                            Arguments = "-jar app.jar",
+                            RedirectStandardOutput = true,
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                        }
+                    };
+                    process.Start();
+
+                    mtex.ReleaseMutex();
+                }
+                else
+                {
+                    Console.Out.WriteLine("An application instance is already running");
+                }
+            }            
+        }
+    }
+    
+}
